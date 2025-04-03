db6011aab7a5e18bc6988be290eacac3a9ba4c2d
monicahcloud <monicahcloud@gmail.com>
2025-04-03 17:24:48 -0500

    trying to deploy working

--- next.config.ts
+++ next.config.ts
@@ -12,9 +12,7 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-  },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "4mb",
