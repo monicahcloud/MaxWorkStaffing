095c0ae66bd31086e5b17a6d5793bf144113cef6
monicahcloud <monicahcloud@gmail.com>
2025-04-07 18:52:51 -0500

    deploying issues

--- app/(dashboard)/editor/page.tsx
+++ app/(dashboard)/editor/page.tsx
@@ -1,20 +1,18 @@
+export const dynamic = "force-dynamic";
 import prisma from "@/lib/prisma";
 import { resumeDataInclude } from "@/lib/types";
 import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 import ResumeEditor from "./ResumeEditor";
 
-interface PageProps {
-  searchParams: Promise<{ resumeId?: string }>;
-}
-
 export const metadata: Metadata = {
   title: "Build your resume",
 };
-export const dynamic = "force-dynamic";
-
+interface PageProps {
+  searchParams: { resumeId?: string };
+}
 export default async function Page({ searchParams }: PageProps) {
-  const { resumeId } = await searchParams;
+  const { resumeId } = searchParams;
 
   const { userId } = await auth();
 
@@ -31,3 +29,31 @@ export default async function Page({ searchParams }: PageProps) {
 
   return <ResumeEditor resumeToEdit={resumeToEdit} />;
 }
+
+// interface PageProps {
+//   searchParams: Promise<{ resumeId?: string }>;
+// }
+
+// export const metadata: Metadata = {
+//   title: "Build your resume",
+// };
+// export const dynamic = "force-dynamic";
+
+// export default async function Page({ searchParams }: PageProps) {
+//   const { resumeId } = await searchParams;
+
+//   const { userId } = await auth();
+
+//   if (!userId) {
+//     return null;
+//   }
+
+//   const resumeToEdit = resumeId
+//     ? await prisma.resume.findUnique({
+//         where: { id: resumeId, userId },
+//         include: resumeDataInclude,
+//       })
+//     : null;
+
+//   return <ResumeEditor resumeToEdit={resumeToEdit} />;
+// }
