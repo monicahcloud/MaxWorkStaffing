885ad0146e1aea565de3c3345bc0d974083d6423
monicahcloud <monicahcloud@gmail.com>
2025-04-13 15:18:47 -0500

    changed landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       </header>
 
       {/* Two-column layout section */}
-      <section className="max-w-7xl mx-auto px-4 md:px-8 flex flex-col md:flex-row items-center justify-center gap-10 min-h-[calc(100vh-150px)]">
+      <section className="max-w-7xl mx-auto px-4 md:px-8 flex flex-col md:flex-row items-center justify-center gap-10 min-h-[calc(100vh-250px)]">
         {/* Left column - Image */}
         <div className="w-full md:w-1/2 flex justify-center">
           <Image
