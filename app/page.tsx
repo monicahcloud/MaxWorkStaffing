37c7ac4f88e1692f34fdffd484cecbc3581b94a7
monicahcloud <monicahcloud@gmail.com>
2025-04-14 16:44:30 -0500

    clerk webhook

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
         <Image
           src={logo}
           alt="Client portal hero image"
-          width={500}
-          height={500}
+          width={450}
+          height={450}
           className="mx-auto -mt-10 mb-5"
         />
         <h1 className="text-5xl font-bold">
@@ -45,14 +45,15 @@ export default function Home() {
         </h3>
       </header>
       <section className="mx-auto justify-center items-center flex flex-col md:flex-row gap-4">
-        <Image
-          src={LandingImg}
-          alt="Client portal hero image"
-          width={700}
-          height={700}
-          className="mx-auto md:ms-0 rounded-2xl shadow-lg justify-center items-center text-center"
-        />
         <div className="flex mx-auto justify-center">
+          <Image
+            src={LandingImg}
+            alt="Client portal hero image"
+            width={700}
+            height={700}
+            className="mx-auto md:ms-0 rounded-2xl shadow-lg justify-center items-center text-center"
+          />
+
           <Button
             asChild
             className="text-3xl rounded-xl px-4 py-2 border border-black bg-red-700 text-white hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
