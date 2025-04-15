39601a40691689be7cf2913893c9771b75e614ad
monicahcloud <monicahcloud@gmail.com>
2025-04-14 21:21:48 -0500

    working on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,8 @@ export default function Home() {
         <h3 className="text-lg text-gray-700">
           Your journey to career success starts here. Build a standout resume,
           stay on top of your job search, and walk into every interview with
-          confidence. Let&apos;s get to work!
+          confidence. <br />
+          Let&apos;s get to work!
         </h3>
       </header>
 
