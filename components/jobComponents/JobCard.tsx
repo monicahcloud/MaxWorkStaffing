0f1eca2ed708e0795a5b9038732ee8e194d9cf4e
monicahcloud <monicahcloud@gmail.com>
2025-04-18 08:46:05 -0500

    working on add jobs

--- components/jobComponents/JobCard.tsx
+++ components/jobComponents/JobCard.tsx
@@ -8,25 +8,35 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Briefcase, RadioTower } from "lucide-react";
+import { Briefcase, CalendarDays, MapPin, RadioTower } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
 import JobInfo from "./JobInfo";
 import DeleteJob from "./DeleteJob";
+import { Separator } from "../ui/separator";
 
 export default function JobCard({ job }: { job: JobType }) {
+  const date = new Date(job.createdAt).toLocaleDateString();
   return (
     <>
       <Card className="bg-muted">
-        <CardHeader>
+        {/* <CardHeader>
           <CardDescription className="uppercase text-xl font-bold">
             {job.location}
           </CardDescription>
           <CardTitle className="capitalize text-2xl">{job.position}</CardTitle>
+        </CardHeader> */}
+        <CardHeader>
+          <CardTitle>{job.position}</CardTitle>
+          <CardDescription>{job.company}</CardDescription>
         </CardHeader>
-        <CardContent className=" gap-4">
+
+        <Separator />
+        <CardContent className="mt-4 grid grid-cols-2 gap-4">
           <JobInfo icon={<Briefcase />} text={job.mode} />
+          <JobInfo icon={<MapPin />} text={job.location} />
+          <JobInfo icon={<CalendarDays />} text={date} />
           {/* <CardDescription>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis
             laboriosam porro, dicta sed inventore tenetur numquam ipsam in et
