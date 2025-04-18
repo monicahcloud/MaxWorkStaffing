59c60b2f0c82cd8a45249b3fa9ee7fa5faa576e8
monicahcloud <monicahcloud@gmail.com>
2025-04-17 21:19:40 -0500

    working on authentication

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,8 @@ const isPublicRoute = createRouteMatcher([
   "/sso-callback(.*)",
   "/api/stripe-webhook",
   "/api/clerk-webhook(.*)",
+  "maxworkstaffing.com",
+  "www.maxworkstaffing.com",
 ]); // Allow "/sign-in" to prevent redirect loops
 
 export default clerkMiddleware(async (auth, req) => {
@@ -17,13 +19,19 @@ export default clerkMiddleware(async (auth, req) => {
   );
 
   // Prevent redirect loop: Don't redirect users who are already on a public page
-  if (!userId && !isPublicRoute(req)) {
-    console.log("Redirecting unauthenticated user to /");
-    return NextResponse.redirect(new URL("/", req.url)); // Redirect to a sign-in page instead
+  // if (!isPublicRoute(req)) {
+  //   console.log("Redirecting unauthenticated user to /");
+  //   return NextResponse.redirect(new URL("/", req.url)); // Redirect to a sign-in page instead
+  // }
+  // Protect private routes - See https://clerk.com/docs/references/nextjs/clerk-middleware#protect-routes-based-on-authentication-status
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
-
-  return NextResponse.next(); // Continue request as normal
+  // Removed the return of NextRequest as there's no need to return anything if the request is valid.
 });
+
+//   return NextResponse.next(); // Continue request as normal
+// });
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"], // Apply middleware to all routes except static files
