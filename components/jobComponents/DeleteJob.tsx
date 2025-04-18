4e56ec0803615e0f8cb0893037406de90c1ccda7
monicahcloud <monicahcloud@gmail.com>
2025-04-18 13:40:41 -0500

    adding pagination

--- components/jobComponents/DeleteJob.tsx
+++ components/jobComponents/DeleteJob.tsx
@@ -1,34 +1,69 @@
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { deleteJobAction } from "@/utils/actions";
+// import { Button } from "@/components/ui/button";
+// import { toast } from "sonner";
+
+// function DeleteJobBtn({ id }: { id: string }) {
+//   const queryClient = useQueryClient();
+//   const { mutate, isPending } = useMutation({
+//     mutationFn: (id: string) => deleteJobAction(id),
+//     onSuccess: (data) => {
+//       if (!data) {
+//         toast("there was an error");
+//         return;
+//       }
+//       queryClient.invalidateQueries({ queryKey: ["jobs"] });
+//       queryClient.invalidateQueries({ queryKey: ["stats"] });
+//       queryClient.invalidateQueries({ queryKey: ["charts"] });
+
+//       toast("job removed");
+//     },
+//   });
+//   return (
+//     <Button
+//       variant="destructive"
+//       size="sm"
+//       disabled={isPending}
+//       onClick={() => {
+//         mutate(id);
+//       }}>
+//       {isPending ? "deleting..." : "delete"}
+//     </Button>
+//   );
+// }
+// export default DeleteJobBtn;
+
+"use client";
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteJobAction } from "@/utils/actions";
-import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Button } from "../ui/button";
 
-function DeleteJobBtn({ id }: { id: string }) {
+function DeleteJob({ id }: { id: string }) {
   const queryClient = useQueryClient();
+
   const { mutate, isPending } = useMutation({
-    mutationFn: (id: string) => deleteJobAction(id),
-    onSuccess: (data) => {
-      if (!data) {
-        toast("there was an error");
-        return;
-      }
+    mutationFn: deleteJobAction,
+    onSuccess: () => {
+      toast.success("Job deleted successfully!");
       queryClient.invalidateQueries({ queryKey: ["jobs"] });
       queryClient.invalidateQueries({ queryKey: ["stats"] });
-      queryClient.invalidateQueries({ queryKey: ["charts"] });
-
-      toast("job removed");
+    },
+    onError: () => {
+      toast.error("Failed to delete job.");
     },
   });
+
   return (
     <Button
-      variant="destructive"
       size="sm"
+      variant="destructive"
       disabled={isPending}
-      onClick={() => {
-        mutate(id);
-      }}>
-      {isPending ? "deleting..." : "delete"}
+      onClick={() => mutate(id)}>
+      {isPending ? "Deleting..." : "Delete"}
     </Button>
   );
 }
-export default DeleteJobBtn;
+
+export default DeleteJob;
