bf0c01005d725db0ccfcf7d2db99e8aa63ade38f
monicahcloud <monicahcloud@gmail.com>
2025-04-18 20:48:32 -0500

    adding support page

--- app/(dashboard)/support/FAQSection.tsx
+++ app/(dashboard)/support/FAQSection.tsx
@@ -104,7 +104,7 @@ export default function FAQPage() {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="w-full mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-center mt-10">
         {faqData.map((cat) => (
           <a
