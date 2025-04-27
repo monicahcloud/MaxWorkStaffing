e8e14e58701d2d7f0fb9b1e78a6535e4092e98d0
monicahcloud <monicahcloud@gmail.com>
2025-04-26 23:15:50 -0500

    its not clerk

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { queryClient } from "@/lib/react-query-client";
 
 function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => {
-    return new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000 * 5,
-        },
-      },
-    });
-  });
+  // const [queryClient] = useState(() => {
+  //   return new QueryClient({
+  //     defaultOptions: {
+  //       queries: {
+  //         staleTime: 60 * 1000 * 5,
+  //       },
+  //     },
+  //   });
+  // });
   return (
     <>
       <ThemeProvider
