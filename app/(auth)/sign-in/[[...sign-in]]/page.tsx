fcefe37b7fff5c6704d0c4ac17f804074be0c9f7
monicahcloud <monicahcloud@gmail.com>
2025-04-26 22:50:40 -0500

    testing

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-3 max-auto mt-10">
-      <SignIn />
+      <div>Sign in here</div>
     </main>
   );
 }
