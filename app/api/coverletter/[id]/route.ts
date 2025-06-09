57377dc5503717126a903c872abb2b12b2eba789
monicahcloud <monicahcloud@gmail.com>
2025-06-08 20:41:18 -0500

    testing stripe

--- app/api/coverletter/[id]/route.ts
+++ app/api/coverletter/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getcoverLetterById } from "@/app/(dashboard)/coverletterbuilder/editor/actions";
+import { getCoverLetterById } from "@/lib/coverletter";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -6,7 +6,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
-  const coverLetter = await getcoverLetterById(id);
+  const coverLetter = await getCoverLetterById(id);
   if (!coverLetter) {
     return NextResponse.json({ error: "Not found" }, { status: 404 });
   }
